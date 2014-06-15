b9cb35265a0e43186ebeaa230f7fdbd751f0bc07
Lalit Sachdeva <sachdeva_libra@yahoo.com>
2014-06-16 00:18:56 +0530

    updated for buttons

--- app/js/router.js
+++ app/js/router.js
@@ -14,37 +14,43 @@ app.controller('Ctrl', function($scope){
             city: 'Amsterdam', 
             bodyText: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.',
             status: '2/4 diploma/certificate/skills',     
-            selected: false      
+            selected: false,
+            value: true
     }); 
 	$scope.Applicants.push({Name:"Lalit Sachdeva",
             job: 'QA Specialist',
             city: 'Delhi', 
             bodyText: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.',
             status: '4/4 diploma/certificate/skills',     
-            selected: true      
+            selected: true,
+            value: true
     }); 
 	$scope.Applicants.push({Name:"Rodger Georgin",
             job: 'QA Engineer',
             city: 'Chicago', 
             bodyText: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.',
             status: '3/4 diploma/certificate/skills',     
-            selected: false      
+            selected: false,
+            value: false    
     }); 
 	$scope.Applicants.push({Name:"George Ducart",
             job: 'Automation Engineer',
             city: 'New York', 
             bodyText: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.',
             status: '2/4 diploma/certificate/skills',     
-            selected: false      
+            selected: false,
+            value: false      
     }); 
 	$scope.Applicants.push({Name:"Benjamin Schuffl",
             job: 'QA Analyst',
             city: 'Netherlands', 
             bodyText: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.',
             status: '3/4 diploma/certificate/skills',     
-            selected: false      
+            selected: false,      
+            value: false
     }); 
 
 	//find number of applicants
 	$scope.lengthOfApplicants = $scope.Applicants.length;
+	$scope
 });
