b94ed734bebfdb3a812a53d3b8fd8dff604314da
Lalit Sachdeva <sachdeva_libra@yahoo.com>
2014-06-23 00:02:15 +0530

    updated table styling

--- app/jobPost/controller.js
+++ app/jobPost/controller.js
@@ -8,7 +8,7 @@ app.controller('jobPostCtrl', function($scope, $filter, ngTableParams){
                 {jobTitle: "Volunteer Coordinator - Volunteer", client:"Posted 4 days ago by Sander Noteborn", Applicants: 34, messaged: 1, hired:1, status:"Open"},
                 {jobTitle: "Crowd Control Volunteering", client:"Posted 6 weeks ago by Sander Noteborn", Applicants: 43, messaged: 3, hired:0, status:"Open"},
                 {jobTitle: "Football Refree Volunteer", client:"Posted 2 months ago by Sander Noteborn", Applicants: 27, messaged: 2, hired:0, status:"Closed"},
-                {jobTitle: "Volunteer Coordinator-Volunteer", client:"Posted 3 weeks ago by Sander Noteborn", Applicants: 34, messaged: 5, hired:3, status:"Open"},
+                {jobTitle: "Volunteering Coordinator", client:"Posted 3 weeks ago by Sander Noteborn", Applicants: 34, messaged: 5, hired:3, status:"Open"},
                 {jobTitle: "Crowd Control Volunteer", client:"Posted 2 weeks ago by Sander Noteborn", Applicants: 43, messaged: 4, hired:2, status:"Closed"},
                 {jobTitle: "Football Refree Volunteering", client:"Posted 1 month ago by Sander Noteborn", Applicants: 34, messaged: 1, hired:0, status:"Closed"},
                 {jobTitle: "Public Relation Intern - Volunteer", client:"Posted 10 days ago by Sander Noteborn", Applicants: 34, messaged: 3, hired:2, status:"Open"}];
@@ -28,7 +28,6 @@ app.controller('jobPostCtrl', function($scope, $filter, ngTableParams){
                                 $filter('orderBy')(data, params.orderBy()) :
                                 data;
             $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-
         }
     });
 });
