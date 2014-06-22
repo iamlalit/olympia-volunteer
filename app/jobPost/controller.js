a0e0b2a0ba8e9d968b93d5ecff14fa101dc11630
Lalit Sachdeva <sachdeva_libra@yahoo.com>
2014-06-22 19:13:15 +0530

    for jobPost

--- app/jobPost/controller.js
+++ app/jobPost/controller.js
@@ -3,21 +3,16 @@
 var app = angular.module('jobPostcontroller', ['ngTable']);
 
 app.controller('jobPostCtrl', function($scope, $filter, ngTableParams){
-	var data = [{jobTitle: "Volunteer Grant Writers", Applicants: 50, messaged: 3, hired:2, status:"Open"},
-                {jobTitle: "Public Relation Intern - Volunteer", Applicants: 43, messaged: 0, hired:0, status:"Open"},
-                {jobTitle: "Outreach Volunteer", Applicants: 27, messaged: 1, hired:0, status:"Closed"},
-                {jobTitle: "Volunteer Article Writings", Applicants: 29, messaged: 2, hired:2, status:"Closed"},
-                {jobTitle: "Volunteer Coordinator - Volunteer", Applicants: 34, messaged: 1, hired:1, status:"Open"},
-                {jobTitle: "Crowd Control Volunteering", Applicants: 43, messaged: 3, hired:0, status:"Open"},
-                {jobTitle: "Football Refree Volunteer", Applicants: 27, messaged: 2, hired:0, status:"Closed"},
-                {jobTitle: "Volunteer Coordinator-Volunteer", Applicants: 29, messaged: 1, hired:1, status:"Closed"},
-                {jobTitle: "Volunteer Coordinator-Volunteer", Applicants: 34, messaged: 5, hired:3, status:"Open"},
-                {jobTitle: "Crowd Control Volunteer", Applicants: 43, messaged: 4, hired:2, status:"Closed"},
-                {jobTitle: "Outreach Volunteering", Applicants: 27, messaged: 5, hired:1, status:"Closed"},
-                {jobTitle: "Volunteer Article Writers", Applicants: 29, messaged: 2, hired:0, status:"Closed"},
-                {jobTitle: "Football Refree Volunteering", Applicants: 34, messaged: 1, hired:0, status:"Closed"},
-                {jobTitle: "Private Relation Intern - Volunteer", Applicants: 43, messaged: 4, hired:0, status:"Open"},
-                {jobTitle: "Public Relation Intern - Volunteer", Applicants: 34, messaged: 3, hired:2, status:"Open"}];
+	var data = [{jobTitle: "Volunteer Grant Writers", client:"Posted 1 month ago by Sander Noteborn" ,Applicants: 50, messaged: 3, hired:2, status:"Open"},
+                {jobTitle: "Outreach Volunteer", client:"Posted 2 years ago by Sander Noteborn", Applicants: 27, messaged: 1, hired:0, status:"Closed"},
+                {jobTitle: "Volunteer Coordinator - Volunteer", client:"Posted 4 days ago by Sander Noteborn", Applicants: 34, messaged: 1, hired:1, status:"Open"},
+                {jobTitle: "Crowd Control Volunteering", client:"Posted 6 weeks ago by Sander Noteborn", Applicants: 43, messaged: 3, hired:0, status:"Open"},
+                {jobTitle: "Football Refree Volunteer", client:"Posted 2 months ago by Sander Noteborn", Applicants: 27, messaged: 2, hired:0, status:"Closed"},
+                {jobTitle: "Volunteer Coordinator-Volunteer", client:"Posted 3 weeks ago by Sander Noteborn", Applicants: 34, messaged: 5, hired:3, status:"Open"},
+                {jobTitle: "Crowd Control Volunteer", client:"Posted 2 weeks ago by Sander Noteborn", Applicants: 43, messaged: 4, hired:2, status:"Closed"},
+                {jobTitle: "Football Refree Volunteering", client:"Posted 1 month ago by Sander Noteborn", Applicants: 34, messaged: 1, hired:0, status:"Closed"},
+                {jobTitle: "Public Relation Intern - Volunteer", client:"Posted 10 days ago by Sander Noteborn", Applicants: 34, messaged: 3, hired:2, status:"Open"}];
+
 
     $scope.tableParams = new ngTableParams({
         page: 1,            // show first page
@@ -32,8 +27,8 @@ app.controller('jobPostCtrl', function($scope, $filter, ngTableParams){
             var orderedData = params.sorting() ?
                                 $filter('orderBy')(data, params.orderBy()) :
                                 data;
-
             $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+
         }
     });
 });
