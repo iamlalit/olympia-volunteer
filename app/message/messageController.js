55a26b23b44767b07dee51d3bae31956fc7349ac
harsh.lamba <harsh.lamba@3pillarglobal.com>
2014-08-13 02:06:05 +0530

    Converted sidebar into repeated

--- app/message/messageController.js
+++ app/message/messageController.js
@@ -1,1 +1,7 @@
-﻿
+﻿var messageApp = angular.module('messageApp', []);
+
+messageApp.controller('messageController', ['$scope', function ($scope) {
+    $scope.messageSidebar = [{'name': 'Inbox'},
+    { 'name': 'Sent' },
+    { 'name': 'Archive' }];
+}]);
