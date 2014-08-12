892d88e771e12113d1858350111f33fa1a435ffe
harsh.lamba <harsh.lamba@3pillarglobal.com>
2014-08-13 02:29:55 +0530

    toggle

--- app/message/messageController.js
+++ app/message/messageController.js
@@ -1,7 +1,38 @@
 ﻿var messageApp = angular.module('messageApp', []);
 
 messageApp.controller('messageController', ['$scope', function ($scope) {
-    $scope.messageSidebar = [{'name': 'Inbox'},
-    { 'name': 'Sent' },
-    { 'name': 'Archive' }];
+    $scope.compose = false;
+    $scope.inbox = false;
+    $scope.send = false;
+    $scope.archive = false;
+    $scope.messageSidebar = [
+        { 'name': 'Inbox' },
+        { 'name': 'Sent' },
+        { 'name': 'Archive' }
+    ];
+    $scope.toggle = function ($index) {
+        if ($index == 0) {
+            $scope.inbox = true;
+            $scope.compose = false;
+            $scope.send = false;
+            $scope.archive = false;
+        }
+        else if ($index == 1) {
+            $scope.send = true;
+            $scope.compose = false;
+            $scope.inbox = false;
+            $scope.archive = false;
+        }
+        else if ($index == 2) {
+            $scope.archive = true;
+            $scope.compose = false;
+            $scope.inbox = false;
+            $scope.send = false;
+        } else {
+            $scope.compose = true;
+            $scope.send = false;
+            $scope.inbox = false;
+            $scope.archive = false;
+        }
+    }
 }]);
