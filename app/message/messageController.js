aac5af2d9776055436c30f6efa37f3707136712c
Lalit Sachdeva <sachdeva_libra@yahoo.com>
2014-08-19 19:54:54 +0530

    change password

--- app/message/messageController.js
+++ app/message/messageController.js
@@ -37,7 +37,100 @@ messageApp.controller('messageController', ['$scope', function ($scope) {
             $scope.archive = false;
         }
     }
+    //dummy data
+    $scope.messageInbox = [];
+    $scope.messageSent = [];
+    $scope.messageArchive = [];
+    $scope.messageInbox.push({name:"Henk-jan Bakker",
+        date: "August 12",
+        checked: false,
+        title: "Lorem ipsum title",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tincidunt, tortor id ultricies eleifend, dui erat dapibus massa, quis aliquet lacus quam id leo."
+    });
+    $scope.messageInbox.push({name:"Jan-Klaassen Bosch",
+        date: "August 1",
+        checked: false,
+        title: "Lorem ipsum title",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tincidunt, tortor id ultricies eleifend, dui erat dapibus massa, quis aliquet lacus quam id leo."
+    });
+    $scope.messageInbox.push({name:"Marleen Dijkstra",
+        date: "June 18",
+        checked: false,
+        title: "Lorem ipsum title",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tincidunt, tortor id ultricies eleifend, dui erat dapibus massa, quis aliquet lacus quam id leo."
+    });
+    $scope.messageInbox.push({name:"Jaap Hendriks",
+        date: "Feburary 4",
+        checked: false,
+        title: "Lorem ipsum title",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tincidunt, tortor id ultricies eleifend, dui erat dapibus massa, quis aliquet lacus quam id leo."
+    });
+    $scope.messageInbox.push({name:"Robin Hendriks",
+        date: "January 29",
+        checked: false,
+        title: "Lorem ipsum title",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tincidunt, tortor id ultricies eleifend, dui erat dapibus massa, quis aliquet lacus quam id leo."
+    });
+    $scope.messageInbox.push({name:"Hendrik Jansen",
+        date: "January 1",
+        checked: false,
+        title: "Lorem ipsum title",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tincidunt, tortor id ultricies eleifend, dui erat dapibus massa, quis aliquet lacus quam id leo."
+    }); 
+    //sent messages
+    $scope.messageSent.push({name:"Geertruda Hendriks",
+        date: "August 3",
+        title: "Lorem ipsum title",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tincidunt, tortor id ultricies eleifend, dui erat dapibus massa, quis aliquet lacus quam id leo."
+    });
+    $scope.messageSent.push({name:"Rob Brouwer",
+        date: "January 10",
+        title: "Lorem ipsum title",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tincidunt, tortor id ultricies eleifend, dui erat dapibus massa, quis aliquet lacus quam id leo."
+    });
+    $scope.messageSent.push({name:"Jan-Klaassen Dijkstra",
+        date: "January 8",
+        title: "Lorem ipsum title",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tincidunt, tortor id ultricies eleifend, dui erat dapibus massa, quis aliquet lacus quam id leo."
+    });
 
+
+    $scope.lengthOfInbox = $scope.messageInbox.length;
+    $scope.lengthOfSent = $scope.messageSent.length;
+    $scope.lengthOfArchive = $scope.messageArchive.length;
+    
+    //function to add messages to archive list
+    $scope.moveMessage = function() {
+        var list = [];
+        for (var i = 0; i < $scope.lengthOfInbox; i++) {
+            if ($scope.messageInbox[i].checked == true) {
+                $scope.messageArchive.push($scope.messageInbox[i]);
+                list.push(i);
+            }
+        };
+        for (var i = list.length-1; i >=0; i--) {
+            console.log(list[i]);
+            $scope.messageInbox.splice(list[i], 1);
+            console.log($scope.messageInbox)
+        };
+        $scope.lengthOfInbox = $scope.messageInbox.length;
+        $scope.lengthOfSent = $scope.messageSent.length;
+        $scope.lengthOfArchive = $scope.messageArchive.length;
+        for (var i = 0; i < $scope.lengthOfArchive; i++) {
+            $scope.messageArchive[i].checked = false;
+        };
+    };
+    //check all to be true
+    $scope.checkAll = function () {
+
+        if($scope.checkall) {
+            console.log("0");
+            for (var i = 0; i < $scope.lengthOfInbox; i++) {
+                $scope.messageInbox[i].checked = true;
+            };  
+        }
+    }
+    //volunteer or organization
     $scope.typeOfOwner = '';
     function getParameterByName(name) {
         name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
